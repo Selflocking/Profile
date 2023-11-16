98f58ff2d59178dcfa16f1a71f9945cdae35b6fa
YunShu <im@yunshu.site>
2023-11-16 21:16:12 +0800

    feat: 添加公安备案图标

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,31 @@
-import React from "react"
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    const year = new Date().getUTCFullYear();
-    return (
-        <footer className="Footer">
-            {`Copyright © ${year} `}
-            <a href='/'>YunShu</a>
-            {" · "}
-            <a href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">冀ICP备2020029697号-1</a>
-            {" · "}
-            <a href="https://beian.mps.gov.cn/#/query/webSearch?code=13050902001780" rel="noreferrer" target="_blank">冀公网安备13050902001780</a>
-        </footer>
-    )
+  const year = new Date().getUTCFullYear();
+  return (
+    <footer className="Footer">
+      <div>
+        {`Copyright © ${year} `}
+        <a href="/">YunShu</a>
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img alt="公安备案图标" src="police_beian.png" width="16px" />
+        &nbsp;
+        <a
+          href="https://beian.mps.gov.cn/#/query/webSearch?code=13050902001780"
+          rel="noreferrer"
+          target="_blank"
+        >
+          冀公网安备13050902001780
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">
+          冀ICP备2020029697号-1
+        </a>
+      </div>
+    </footer>
+  );
 }
 
-
-export default Footer
+export default Footer;
